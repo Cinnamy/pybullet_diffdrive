f84fed1df4bd60a2efc7925cd9cf5f47491a538b
Marina Lysyuk <lysyuk.m.o@gmail.com>
2025-12-24 01:59:49 +0300

    Fixes to improve performance

--- run_new.py
+++ run_new.py
@@ -23,22 +23,19 @@ pb.resetDebugVisualizerCamera(
 # wheel order: [right, left]
 motorIdx = [0, 1]
 
-# Параметры управления
 RAY_LENGTH = 30.0
-SPEED = 40.0
-DISTANCE_45 = 1.4
-DISTANCE_90 = 1.0
+SPEED = 30.0
+DISTANCE_45 = 2.3
+DISTANCE_0 = 1.5
+DISTANCE_90 = 1
 
-# Функция для расчета скоростей колес
 def calculate_wheel_speeds(dist_0, dist_45, dist_90, dist_135):
     d = 10.0
 
-    # [правое колесо, левое колесо]
-
-    if dist_0 < DISTANCE_90 or dist_45 < DISTANCE_45:
+    if dist_0 < DISTANCE_0 or dist_45 < DISTANCE_45:
         return [SPEED, d]
 
-    if dist_90 == 30.0 or dist_45 == 30.0 or dist_90 > DISTANCE_45:
+    if dist_90 == RAY_LENGTH or dist_45 == RAY_LENGTH or dist_90 > DISTANCE_90:
         return [d, SPEED]
     
     return [SPEED, SPEED]
@@ -52,8 +49,7 @@ pb.setJointMotorControl2(
     force=0,
 )
 
-
-maxTime = 20 
+maxTime = 100 
 dt = 1 / 60
 logTime = np.arange(0, maxTime, dt)
 
@@ -70,45 +66,36 @@ for t in logTime:
 
     pos, orn = pb.getBasePositionAndOrientation(obj)
     euler = pb.getEulerFromQuaternion(orn)
-    yaw = euler[2]
-    
-    ray_distances = []
-    ray_hit_points = []
+    angle = euler[2]
     
     # measure the distance to obstacles with rays
-    for i, angle_local in enumerate(angles_local):
-        ray_angle = yaw + angle_local
-        pos_from = [pos[0], pos[1], pos[2] + 0.1]
-        pos_to = [
-            pos[0] + RAY_LENGTH * np.cos(ray_angle),
-            pos[1] + RAY_LENGTH * np.sin(ray_angle),
-            pos[2] + 0.1
-        ]
-        
-        ray_result = pb.rayTest(pos_from, pos_to)[0]
-        hit_fraction = ray_result[2]
-        hit_position = ray_result[3]
-        
-        distance = hit_fraction * RAY_LENGTH
-        ray_distances.append(distance)
-        ray_hit_points.append(hit_position)
 
-        ray_end = [
-            pos[0] + RAY_LENGTH * np.cos(ray_angle),
-            pos[1] + RAY_LENGTH * np.sin(ray_angle),
-            pos[2] + 0.1
-        ]
-        
-        ray_result = pb.rayTest(pos_from, ray_end)[0]
-        hit_fraction = ray_result[2]
-        hit_position = ray_result[3]
+    ray_angles = [angle + np.pi/2, angle + np.pi/4, angle, angle - np.pi/4]
 
+    pos_from = np.tile([pos[0], pos[1], pos[2] + 0.1], (4,1))
+
+    ray_end_x = pos[0] + RAY_LENGTH * np.cos(ray_angles)
+    ray_end_y = pos[1] + RAY_LENGTH * np.sin(ray_angles)
+    ray_end_z = np.full(4, pos[2] + 0.1)
+    pos_to = np.vstack((ray_end_x, ray_end_y, ray_end_z)).T
+
+    results = pb.rayTestBatch(
+        pos_from,
+        pos_to
+    )
+
+    hit_fractions = [ray[2] for ray in results]
+    distances = np.array(hit_fractions) * RAY_LENGTH
+
+    hit_positions = [ray[3] for ray in results]
+
+    for i, hit_fraction in enumerate(hit_fractions):
         if hit_fraction < 1.0:
-            pb.addUserDebugLine(pos_from, hit_position, ray_colors[i], lifeTime=0.1)
+            pb.addUserDebugLine(pos_from[i], hit_positions[i], ray_colors[i], lifeTime=0.1)
         else:
-            pb.addUserDebugLine(pos_from, ray_end, [0.5, 0.5, 0.5], lifeTime=0.1)
+            pb.addUserDebugLine(pos_from[i], pos_to[i], [0.5, 0.5, 0.5], lifeTime=0.1)
     
-    dist_0, dist_45, dist_90, dist_135 = ray_distances
+    dist_0, dist_45, dist_90, dist_135 = distances
 
     speeds = calculate_wheel_speeds(dist_0, dist_45, dist_90, dist_135)
     
